c42f09767038c1763b55024d8236f1af157de3af
moesarsour <SarsourM@gmail.com>
2023-02-27 12:09:48 -0500

    New Places can be Added

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const places = require('../models/places')
+const places = require('../models/places.js')
 
 router.get('/', (req, res) => {
     res.render('places/index', {places})
@@ -11,7 +11,17 @@ router.get('/new', (req, res) => {
 
 router.post('/', (req, res) => {
     console.log(req.body)
-    res.send('POST /places')
+    if (!req.body.pic) {
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
   })
   
 
