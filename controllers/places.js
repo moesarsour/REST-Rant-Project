7bbb345bf01a95c9c65f69cc23bc286658a21e31
moesarsour <SarsourM@gmail.com>
2023-03-02 02:38:08 -0500

    Added the Edit Functionality

--- controllers/places.js
+++ controllers/places.js
@@ -26,7 +26,28 @@ router.get('/:id/edit', (req, res) => {
   }
 })
 
-
+router.put('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+      if (!req.body.pic) {
+          req.body.pic = 'http://placekitten.com/400/400'
+      }
+      if (!req.body.city) {
+          req.body.city = 'Anytown'
+      }
+      if (!req.body.state) {
+          req.body.state = 'USA'
+      }
+      places[id] = req.body
+      res.redirect(`/places/${id}`)
+  }
+})
 
 router.post('/', (req, res) => {
     console.log(req.body)
