5b183f8a02a7c466a3691e3611f7eb8cd052f872
moesarsour <SarsourM@gmail.com>
2023-03-08 18:39:14 -0500

    Added Comments section and implement functionality

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -21,4 +21,39 @@ function new_form (data) {
     )
 }
 
+function show (data) {
+  let comments = (
+    <h3 className="inactive">
+      No comments yet!
+    </h3>
+  )
+  if (data.place.comments.length) {
+    comments = data.place.comments.map(c => {
+      return (
+        <div className="border">
+          <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <stong>- {c.author}</stong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      )
+    })
+  }
+  return (
+      <Def>
+        <main>
+          <div className="row">
+            ...
+          </div>
+          <hr />
+          <h2>Comments</h2>
+          {comments}
+        </main>
+      </Def>
+  )
+}
+
+module.exports = show
 module.exports = new_form
